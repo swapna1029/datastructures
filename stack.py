7099bfd9a253a64b6a30eb667c5e9ee3f068ccc9
KARTHIKEYAN.S.A <53579498+karthikeyansa@users.noreply.github.com>
2019-08-16 09:11:12 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,26 @@
+class stack:
+    def __init__(self):
+        self.stack=[]
+    def add (self,data):
+        if data not in self.stack:
+            self.stack.append(data)
+            return True
+        else:
+            return False#add is mot used here
+    def peek():
+        return self.stack[-1]
+    def push(self,data):
+        self.stack.append(data)
+        return self.stack
+    def remove(self):
+        if len(self.stack)<=0:
+            return 'no element'
+        else:
+            return self.stack.pop()
+    def print(self):
+        print( self.stack)
+s=stack()
+s.push(4)
+s.push(5)
+s.push(6)
+s.print()
